02fdd874891f2a5c76ff983a0c65ac056697da7a
Tyler Roach <tylerjroach@gmail.com>
2018-05-15 22:12:45 -0400

    solution to enter and exit immersive view while resizing the view properly

--- app/src/main/java/net/bradball/android/immersivedemo/MainActivity.java
+++ app/src/main/java/net/bradball/android/immersivedemo/MainActivity.java
@@ -2,12 +2,13 @@ package net.bradball.android.immersivedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private Toolbar toolbar;
     private Button mUIToggle;
 
     private boolean mIsImmersiveEnabled = false;
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         mUIToggle = findViewById(R.id.ui_toggle);
         mUIToggle.setOnClickListener(new View.OnClickListener() {
@@ -31,13 +34,11 @@ public class MainActivity extends AppCompatActivity {
         View decorView = getWindow().getDecorView();
 
         if (mIsImmersiveEnabled) {
-            // Immersive Mode is on, turn it off.
-            decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                  | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                  | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            toolbar.setVisibility(View.VISIBLE);
+            decorView.setSystemUiVisibility(View.VISIBLE);
         } else {
             // Immersive Mode is off, turn it on.
+            toolbar.setVisibility(View.GONE);
             decorView.setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_IMMERSIVE
                     // Set the content to appear under the system bars so that the
